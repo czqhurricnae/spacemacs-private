854be0e29464a208db34e6527658338ef5676e8d
c <7160844@qq.com>
2019-11-16 16:30:00 +0800

    对 rjsx mode 的缩进进行更详细设置

--- layers/hurricane-programming/funcs.el
+++ layers/hurricane-programming/funcs.el
@@ -43,8 +43,13 @@
   ;; (js2-minor-mode)
   (when (equal "js" (file-name-extension (or (buffer-file-name) "")))
       (js2-mode))
+  (setq css-indent-offset 2)
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
+  (setq web-mode-attr-indent-offset 2)
+  (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
   (emmet-mode))
 ;; }}

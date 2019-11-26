28c3f85f17894e4793a5c7385f112ad8d1731473
c <7160844@qq.com>
2019-11-26 20:52:17 +0800

    让讨厌的 Backtrack buffer 弹窗消失

--- init.el
+++ init.el
@@ -427,6 +427,7 @@ you should place your code here."
   ;; (setq url-user-agent "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; WOW64; Trident/4.0; SLCC1)")
   ;; (set-face-attribute 'default nil :family "Source Code Pro")
   ;; (set-face-attribute 'default nil :height 165)
+  (spacemacs/toggle-debug-on-error-off)
  )
 (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
 (load custom-file 'no-error 'no-message)

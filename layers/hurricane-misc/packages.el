9ea612f3204be1458a170f055678ec2a74161bf9
c <7160844@qq.com>
2020-06-09 17:43:37 +0800

    修复 multiple-cursors-hydra 菜单不对齐

--- layers/hurricane-misc/packages.el
+++ layers/hurricane-misc/packages.el
@@ -424,11 +424,11 @@
     (defhydra multiple-cursors-hydra (:hint nil)
       "
        ^Up^            ^Down^        ^Other^
-             ----------------------------------------------
-         [_p_]   Next    [_n_]   Next    [_l_] Edit lines
-         [_P_]   Skip    [_N_]   Skip    [_a_] Mark all
-         [_M-p_] Unmark  [_M-n_] Unmark [_r_] Mark by regexp
-         ^ ^             ^ ^ [_q_] Quit
+       -----------------------------------------------
+       [_p_]   Preview [_n_]   Next   [_l_] Edit lines
+       [_P_]   Skip    [_N_]   Skip   [_a_] Mark all
+       [_M-p_] Unmark  [_M-n_] Unmark [_r_] Mark by regexp
+       ^ ^             ^ ^ [_q_] Quit
        "
       ("l" mc/edit-lines :exit t)
       ("a" mc/mark-all-like-this :exit t)

4b39d1e4f214d118c17152c1d45512eb53a75c50
c <7160844@qq.com>
2020-06-12 00:49:21 +0800

    删除 all-the-icons-dired 包

--- layers/hurricane-ui/packages.el
+++ layers/hurricane-ui/packages.el
@@ -11,7 +11,6 @@
                             github :repo "manateelazycat/awesome-tab"))
     (shackle :location (recipe :fetcher
                         github :repo "wasamasa/shackle"))
-    all-the-icons-dired
     (ivy-posframe :location (recipe :fetcher
                              github :repo "tumashu/ivy-posframe"))
     ))
@@ -243,43 +242,6 @@
   (and hurricane-icon
        (display-graphic-p)
        (require 'all-the-icons nil t)))
-
-(defun hurricane-ui/init-all-the-icons-dired ()
- (use-package all-the-icons-dired
-    :diminish
-    :if (icons-displayable-p)
-    :hook (dired-mode . all-the-icons-dired-mode)
-    :config
-    ;; FIXME: Refresh after creating or renaming the files/directories.
-    ;; @see https://github.com/jtbm37/all-the-icons-dired/issues/34.
-    (with-no-warnings
-      (advice-add 'dired-do-create-files :around #'all-the-icons-dired--refresh-advice)
-      (advice-add 'dired-create-directory :around #'all-the-icons-dired--refresh-advice))
-
-    (with-no-warnings
-      (defun my-all-the-icons-dired--refresh ()
-        "Display the icons of files in a dired buffer."
-        (all-the-icons-dired--remove-all-overlays)
-        ;; NOTE: don't display icons it too many items
-        (if (<= (count-lines (point-min) (point-max)) 1000)
-            (save-excursion
-              (goto-char (point-min))
-              (while (not (eobp))
-                (when (dired-move-to-filename nil)
-                  (let ((file (file-local-name (dired-get-filename 'relative 'noerror))))
-                    (when file
-                      (let ((icon (if (file-directory-p file)
-                                      (all-the-icons-icon-for-dir file
-                                                                  :face 'all-the-icons-dired-dir-face
-                                                                  :height 0.9
-                                                                  :v-adjust all-the-icons-dired-v-adjust)
-                                    (all-the-icons-icon-for-file file :height 0.9 :v-adjust all-the-icons-dired-v-adjust))))
-                        (if (member file '("." ".."))
-                            (all-the-icons-dired--add-overlay (point) "  \t")
-                          (all-the-icons-dired--add-overlay (point) (concat icon "\t")))))))
-                (forward-line 1)))
-          (message "Not display icons because of too many items.")))
-      (advice-add #'all-the-icons-dired--refresh :override #'my-all-the-icons-dired--refresh))))
 
 (defun hurricane-ui/init-ivy-posframe ()
   (use-package ivy-posframe
